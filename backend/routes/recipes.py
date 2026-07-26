71f2bd52f36be1d4659f4a3eccd211eea99ba86b
Jondrielle <Jondriellewilson@gmail.com>
2026-07-25 22:00:26 -0500

    Completed post and delete endpoint for recipes

--- backend/routes/recipes.py
+++ backend/routes/recipes.py
@@ -1,20 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
+from schemas.recipe import CreateRecipe,Recipe
 
 recipe_router = APIRouter()
 
 recipes = []
+recipeID = 0
 
 @recipe_router.get("/")
 async def get_recipes():
-	return {"recipes"}
+	return recipes
 
-@recipe_router.post("/recipe")
-async def add_recipe():
-	return "Created recipe"
+@recipe_router.post("/recipe", response_model=Recipe)
+async def add_recipe(recipe:CreateRecipe):
+	global recipeID 
 
-@recipe_router.delete("/recipe")
-async def delete_recipe():
-	return "Delete recipe"
+	new_recipe = Recipe(
+		id = recipeID,
+		name = recipe.name,
+		description = recipe.description,
+		ingredients = recipe.ingredients,
+		instructions = recipe.instructions,
+		prep_time = recipe.prep_time,
+		cook_time = recipe.cook_time,
+		servings = recipe.servings,
+		difficulty = recipe.difficulty
+	)
+
+	recipes.append(new_recipe)
+	recipeID += 1
+
+	return new_recipe
+
+@recipe_router.delete("/recipe{id}")
+async def delete_recipe(id: int):
+	for recipe in recipes:
+		if recipe.id == id:
+			recipes.remove(recipe)
+			return {"Message:","Recipe Deleted Successfully"}
+	
+	raise HTTPException(
+		status_code = 404,
+		detail = "Recipe Not Found"
+	)
 
 @recipe_router.patch("/recipe{id}")
 async def update_recipe():
--- backend/schemas/recipe.py
+++ backend/schemas/recipe.py
@@ -1,13 +1,27 @@
-from pydantic import BaseModel,Optional
+from pydantic import BaseModel
+from typing import Optional
 from enums.difficulty import Difficulty
 
-
 class Recipe(BaseModel):
+	id: int
 	name: str
 	description:Optional[str] = None
-	ingredients:str[]
+	ingredients:list[str]
 	instructions:str
-	prep time:str
-	cook time:str
+	prep_time:int
+	cook_time:int
 	servings:int
 	difficulty: Difficulty
+
+class CreateRecipe(BaseModel):
+	name: str
+	description:Optional[str] = None
+	ingredients:list[str]
+	instructions:str
+	prep_time:int
+	cook_time:int
+	servings:int
+	difficulty: Difficulty
+
+class RecipeResponse(BaseModel):
+	id: int
